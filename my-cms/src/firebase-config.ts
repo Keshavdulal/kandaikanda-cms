7e13f16400decabadda2ce4e6dc8b84019d815fc
anticipator1 <rowan.tamang@gmail.com>
2025-02-23 09:47:52 +0545

    feat: Add initial configuration files and components

---
+++ my-cms/src/firebase-config.ts
@@ -0,0 +1,10 @@
+// TODO: Replace with your config
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_APIKEY,
+  authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
+};
